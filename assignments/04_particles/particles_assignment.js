ab41f463e9ed6cd4915f978ebb44efa297eaa948
GooberGirl15 <120996231+GooberGirl15@users.noreply.github.com>
2024-02-26 11:30:21 -0500

    bigger

--- assignments/04_particles/particles_assignment.js
+++ assignments/04_particles/particles_assignment.js
@@ -1,11 +1,9 @@
 let gravity;
-let picker;
 
 function setup(){
-    mainCanvas = createCanvas(500,400);
     waterfall = new ParticleSystem(250,200);
     gravity = createVector(0,0.01);
-    airCanvas = createCanvas(500,400);
+    airCanvas = createCanvas(800,700);
     
 
 
